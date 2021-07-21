dc0c305dd2501ebbd598e07d6c0c113947b7b1df
ApollosSevere <55603364+ApollosSevere@users.noreply.github.com>
2021-07-21 08:47:00 -0400

    ProductDetail Slider

--- client/src/components/productDetail/ProductDetail.jsx
+++ client/src/components/productDetail/ProductDetail.jsx
@@ -18,12 +18,95 @@ function ProductDetail() {
                                     <li data-thumb="images/large-img-2.jpg"> <img class="img-responsive" src="images/large-img-2.jpg"  alt=""/> </li>
                                     <li data-thumb="images/large-img-3.jpg"> <img class="img-responsive" src="images/large-img-3.jpg"  alt=""/> </li>
                                 </ul>
-                                <div className="featured">
-                                    <span className="featured-Item"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></span>
-                                    <span className="featured-Item"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></span>
-                                    <span className="featured-Item"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></span>
+                                <div className="photos d-flex align-items-center justify-content-center ">
+                                     <div className="featured d-flex align-items-center">
+                                        <div className="featured-Item"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></div>
+                                        <div className="featured-Item active"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></div>
+                                        <div className="featured-Item"><img class="img-responsive" src="images/large-img-2.jpg"  alt=""/></div>
+                                    </div>
                                 </div>
+                               
                             </div>
+                            
+                            {/* <div class="images-slider">
+                                <ul class="slides">
+                                    <li
+                                    data-thumb="images/large-img-1.jpg"
+                                    class="flex-active-slide"
+                                    data-thumb-alt=""
+                                    style={{
+                                        width: '100%',
+                                        float: 'left',
+                                        marginRight: '-100%',
+                                        position: 'relative',
+                                        opacity: '1',
+                                        display: 'block',
+                                        zIndex: '2',
+                                    }}
+                                    >
+                                    <img
+                                        class="img-responsive"
+                                        src="images/large-img-1.jpg"
+                                        alt=""
+                                        draggable="false"
+                                    />
+                                    </li>
+                                    <li
+                                    data-thumb="images/large-img-2.jpg"
+                                    data-thumb-alt=""
+                                    style={{
+                                        width: '100%',
+                                        float: 'left',
+                                        marginRight: '-100%',
+                                        position: 'relative',
+                                        opacity: '0',
+                                        display: 'block',
+                                        zIndex: '1',
+                                    }}
+                                    >
+                                    <img
+                                        class="img-responsive"
+                                        src="images/large-img-2.jpg"
+                                        alt=""
+                                        draggable="false"
+                                    />
+                                    </li>
+                                    <li
+                                    data-thumb="images/large-img-3.jpg"
+                                    data-thumb-alt=""
+                                    style={{
+                                        width: '100%',
+                                        float: 'left',
+                                        marginRight: '-100%',
+                                        position: 'relative',
+                                        opacity: '0',
+                                        display: 'block',
+                                        zIndex: '1',
+                                    }}
+                                    >
+                                    <img
+                                        class="img-responsive"
+                                        src="images/large-img-3.jpg"
+                                        alt=""
+                                        draggable="false"
+                                    />
+                                    </li>
+                                </ul>
+                                <ol class="flex-control-nav flex-control-thumbs">
+                                    <li>
+                                        <img src="images/large-img-1.jpg" class="flex-active" draggable="false" />
+                                    </li>
+                                    <li><img src="images/large-img-2.jpg" draggable="false" /></li>
+                                    <li><img src="images/large-img-3.jpg" draggable="false" /></li>
+                                </ol>
+
+                                <ul class="flex-direction-nav">
+                                    <li class="flex-nav-prev"><a class="flex-prev" href="#">Previous</a></li>
+                                    <li class="flex-nav-next"><a class="flex-next" href="#">Next</a></li>
+                                </ul>
+                                
+                            </div>
+                         */}
                         </div>
 
                         {/* Content */}
